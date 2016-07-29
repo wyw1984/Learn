aeca5ef84337ed5197514ce70eae668a7c68d467
weiyanwu <yanwu.wei@zerotech.com>
2016-07-29 09:52:12 +0800

    Init Simple

---
+++ TestSimpleJSPatch/TestSimpleJSPatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSimpleJSPatch
+//
+//  Created by weiyanwu on 16/7/29.
+//  Copyright © 2016年 weiyanwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
